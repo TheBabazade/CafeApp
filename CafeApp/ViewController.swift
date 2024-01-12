b53daed022893a02826587968d7b7119f7564714
Nail Babazadə <123533753+TheBabazade@users.noreply.github.com>
2024-01-12 17:12:13 +0400

    ınıtıal commıt

---
+++ CafeApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CafeApp
+//
+//  Created by Baba on 12.01.24.
+//
+
+import UIKit
+import Firebase
+
+
+class ViewController: UIViewController {
+    @IBOutlet weak var emailText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signInClicked(_ sender: Any) {
+    }
+    @IBAction func signUpClicked(_ sender: Any) {
+    }
+    
+}
+
